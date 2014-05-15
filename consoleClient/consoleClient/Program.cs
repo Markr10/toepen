5f892d33cc1a908c4d374607263de5430ffa31db
raymonnetje <raymon.bunt@student.stenden.com>
2014-05-15 13:18:46 +0200

    server/client

---
+++ consoleClient/consoleClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace consoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                TcpClient tcpclnt = new TcpClient();
+                 
+                Console.WriteLine(">> Connecting");
+
+                //same ip as in server programm
+                tcpclnt.Connect("10.111.200.73", 5431);
+
+                Console.WriteLine(">> Connected...");
+
+
+
+                Console.WriteLine(tcpclnt.Client.LocalEndPoint);
+                    Console.Write(">> Enter the string to be sent: ");
+
+                    String str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+
+
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        byte[] ba = asen.GetBytes(str);
+                        Console.WriteLine(">> Sending...");
+
+                        stm.Write(ba, 0, ba.Length);
+
+                        byte[] bb = new byte[100];
+
+                        int k = stm.Read(bb, 0, 100);
+
+                        for (int i = 0; i < k; i++)
+                        {
+                            Console.Write(Convert.ToChar(bb[i]));
+                        }
+
+                        
+                  
+                        tcpclnt.Close();
+                       
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error.... {0}", e.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ consoleServer/consoleServer/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace consoleServer
+{
+    class Program
+    {
+        private static Thread serverThread;
+        private static Thread exitThread;
+        private static IPAddress ipAdress;
+        private static TcpListener myList;
+        private static string serverIP;
+
+
+        public static void Main(string[] args)
+        {
+            serverIP = "10.111.200.73"; // getIp();
+
+            ipAdress = IPAddress.Parse(serverIP);
+
+            // Initializes the Listener
+            myList = new TcpListener(ipAdress, 5431);
+
+            serverThread = new Thread(runServer);
+            exitThread = new Thread(stopServer);
+            exitThread.Start();
+            serverThread.Start();
+            
+        }
+
+        public static void stopServer()
+        {
+            while (true)
+            {
+                String str = Console.ReadLine();
+
+                if (str == "exit")
+                {
+                    myList.Stop();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Command not found!");
+                }
+            }
+        }
+
+        public static void runServer()
+        {
+            
+            try
+            {
+
+                
+
+                // Start listening
+                myList.Start();
+
+                Console.WriteLine(">> Server running.....");
+                Console.WriteLine(">> Ip address: " + serverIP);
+                Console.WriteLine(">> Port: 5431");
+                Console.WriteLine(">> Waiting for connections...");
+
+                while(true)
+                {
+                    Socket s = myList.AcceptSocket();
+
+                    
+                    //when accepted
+                    Console.WriteLine("Connection accepted from " + s.LocalEndPoint);
+
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    Console.WriteLine(">> Received...");
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        Console.Write(Convert.ToChar(b[i]));
+                    }
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("Automatic message: String received by server!"));
+                    Console.WriteLine("\n Automatic message sent!");
+
+
+                    s.Close();
+                    
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error.... ", e.StackTrace);
+            }
+        }
+
+
+        public static string getIp()
+        {
+            string ip = "";
+            string strHostName = "";
+            strHostName = System.Net.Dns.GetHostName();
+
+            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+
+            IPAddress[] addr = ipEntry.AddressList;
+
+            ip = addr[2].ToString();
+
+            return ip;
+        }
+    }
+}
